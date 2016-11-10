60ea1f8307078c742301568f4d82963372a356e3
Erik Lilja <erik.lilja@ericsson.com>
2016-11-10 13:11:04 +0100

    Add basic funcionality

--- bin/hour.bash
+++ bin/hour.bash
@@ -23,6 +23,8 @@ function months_of_the_year
 		month=$(date "+%h" "-d ${start_date}-${i_c}-01")
 		monthly[$month]=0
 		months[$i]=$month
+		brutto[$i]=0
+		netto[$i]=0
 		i=$i_c
 	done
 }
@@ -73,15 +75,78 @@ function prompt_user
 	done
 }
 
+
 function hours_to_brutto_pay
 {
-	
+	hour_plain=0
+	vacation_replacement_payment=0
+	lunch_replacement_payment=0
+
+	for month in "${!months[@]}"
+	do
+		hours=${monthly[${months[$month]}]}
+		if [ -z "${monthly[${months[$month]}]}" ]
+		then
+			hours=0
+		fi
+
+		month_name=${months[$month]}
+		echo "-----------------"
+		echo "Month: $month_name. Monthly hours: $hours"
+
+		hour_plain=$((112*$hours))
+		percentage=12
+		total=100
+		vacation_replacement_payment=$(awk "BEGIN {print ($percentage/$total)}") 
+		vacation_replacement_payment=$(awk "BEGIN {print ($vacation_replacement_payment*$hour_plain)}" )
+		lunch_replacement_payment=$(awk "BEGIN {print (5.68*$hours) }" )
+
+		brutto[$month]=$(awk "BEGIN { print ($hour_plain+$vacation_replacement_payment+$lunch_replacement_payment) }")
+
+		echo "hour: $hour_plain"
+		echo "vacation: $vacation_replacement_payment"
+		echo "lunch: $lunch_replacement_payment"
+		echo "Total: ${brutto[$month]}"
+	done
+
+}
+
+function brutto_to_netto
+{
+	# Tax = 33 %
+	tax=33
+
+	# float results = OriginalValue - (OriginalValue * Percent / 100)
+	for month in "${!brutto[@]}"
+	do
+		month_hour=${brutto[$month]}
+		netto[$month]=$(awk "BEGIN { print ($month_hour - ($month_hour * $tax / 100) ) }")
+		echo "Brutto:${brutto[$month]}.	Netto:${netto[$month]}" 
+	done
+
+}
+
+function total_brutto_and_netto
+{
+	total_brutto=0
+	total_netto=0
+
+	for month in "${!brutto[@]}"
+	do
+		total_brutto=$(awk "BEGIN {print ($total_brutto+${brutto[$month]}) }")
+		total_netto=$(awk "BEGIN {print ($total_netto+${netto[$month]}) }")
+	done
+
+	echo "Total brutto: $total_brutto	Total Netto: $total_netto"
 }
 
 function main
 {
 	init
 	prompt_user
+	hours_to_brutto_pay
+	brutto_to_netto
+	total_brutto_and_netto
 
 	while [ "$#" -gt 0 ]
 	do
