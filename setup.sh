687a32baa859036575856df2fb724203ce150b9d
Erik Lilja <erik.liljaa@live.se>
2016-12-10 02:34:08 +0100

    Copy support
    Adds copy support with the --no-ln flag
    This will use cp instead of ln

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ usage()
 }
 
 # Helper functions
-if [ -f "$sourcedir/dotbin.symlink/colors.sh" ] 
+if [ -f "$sourcedir/dotbin.symlink/colors.sh" ]
 then
 	. "$sourcedir/dotbin.symlink/colors.sh"
 else
@@ -89,7 +89,7 @@ read_char() {
 
 # See if vim is installed
 viminstall() {
-	vim=$(command -v vim 2>/dev/null) 
+	vim=$(command -v vim 2>/dev/null)
 	if [ -d "$vim" ] || [ -f "$vim" ]
 	then
 		success "Vim installed"
@@ -104,6 +104,7 @@ uninstall_dot_files()
 	local mode="$1"
 	local verbose=$2
 	local wd=$(echo "$PWD")
+	local no_ln=$3
 	cd "$symtarget"
 
 	# Get all dotfiles in symtarget
@@ -118,7 +119,7 @@ uninstall_dot_files()
 		target=$(ls -al "$file" | awk '{print $11}')
 		is_target_dotfile_repo=$(echo "$target" | grep "$sourcedir") # if the symlink of the current dotfile links to "*../dotfiles/*"
 
-		if [ ! -z "$target" ] && [ "$target" = "$is_target_dotfile_repo" ] 
+		if [ ! -z "$target" ] && [ "$target" = "$is_target_dotfile_repo" ]
 		then # match
 			if [ "$mode" = $ASK ]
 			then
@@ -136,6 +137,49 @@ uninstall_dot_files()
 			fi # no ask mode, just delete
 		else
 			test "$verbose" -eq 1 && info "File was not in dotfile repo"
+
+			# are we running no_ln mode? Is the files in target directory copied instead of linked?
+			if [ $no_ln -eq 1 ]
+			then
+				if [ -f "$file" ]
+				then
+					# We are. Check if there is a file with the same hash in the dotfile repo. Sure this means
+					# that it's ours to delete.
+
+					k=$(find "$sourcedir" | grep "$file" | xargs sha1sum)
+					file_name_arr=$(echo "$source_file" | awk '{print $1}')
+					hash_arr=$(echo "$source_file" | awk '{print $1}')
+
+					iterator=0
+					n_iterator=1
+					len=$(echo "$k" | wc -l)
+
+					while [ $iterator -lt $len ]
+					do
+						line=$(echo "$k" | head -n+"${n_iterator}" | tail -n1)
+						source_file_hash=$(echo "$line" | awk '{print $1}')
+						source_file_name=$(echo "$line" | awk '{print $2}')
+						current_file_hash=$(sha1sum "$file" | awk '{print $1}')
+
+						if [ "$source_file_hash" = "$current_file_hash" ]
+						then
+							# info "$source_file"
+							info "$file_p $source_p"
+							prompt "File ${symtarget}/${file}(${current_file_hash}) has the same hash value as ${source_file_name}. Delete ${symtarget}/${file}(${source_file_hash})?"
+							read_char x
+
+							if [ $x = $AGREE ]
+							then
+								rm "$file"
+							fi
+						fi
+						((iterator+=1))
+						((n_iterator+=1))
+					done
+				fi
+
+
+			fi
 		fi # ! -z $target
 	done
 
@@ -174,38 +218,131 @@ install_dot_file() {
 	local mode="$3"
 	local verbose="$4"
 	local valid="$5"
+	local no_ln=$6
+	local target_exists=0
+	local same_diff=0
+	local type_of_file=-1 # 0=symlink, 1=file, 2=directory
+	local dest_type_of_file=-1 # 0=symlink, 1=file, 2=directory
 
 	if [ ! -z "$dest" ] || [ ! -z "$link" ]
-	then	
+	then
 		install=0
 		skip=0
 		backup=0
 
-		if ! test -h "$link"
-		then
-			install=1
-		fi
+		# First, check if we even want to install the thing. If the user has -a flag that is.
+		if [ "$mode" = $ASK ]
+			then
+				x=""
+				destcopy=$dest
 
-		if [ "$install" -eq 0 ]
+				destcopy=$(basename $destcopy)
+				str=""
+				if [ -d "$dest" ]
+				then
+					str="Do you want to install the directory '$destcopy/'? ($valid more to go)"
+				else
+					str="Do you want to install the file '$destcopy'? ($valid more to go)"
+				fi
+				prompt "$str"
+				read_char x
+				echo ""
+				if [ "$x" = $AGREE ]
+				then
+					install=1
+				else
+					install=0
+				fi
+			fi
+
+		# Test the dest file, what type of target is it?
+		if [ -f "$dest" ]
 		then
-			sym_link=$(readlink "$link") 
+			dest_type_of_file=1
+		elif [ -d "$dest" ]
+		then
+			dest_type_of_file=2
+		fi
+		# Check if the link exists, if it's a file or symlink.
+
+		if test -h "$link"
+		then
+			sym_link=$(readlink "$link")
+			type_of_file=0 # symlink
+			target_exists=1
+
 			if [ "$sym_link" = "$dest" ] # would an installation point to the same as existing?
 			then
 				info "Skipping '$dest' because the existing link would link to same post-install"
+				target_exists=1
 				skip=1
 			fi
-
-			if [ "$skip" -eq 0 ]
+		elif [ $no_ln -eq 1 ]
+		then
+			if [ -e "$link" ]
 			then
-				info "There seems like '$link' is already a symbolic link and does not link to ${sourcedir}. What would you like to do?"
+				target_exists=1
+
+				if [ -f "$link" ]
+				then
+					type_of_file=1 # file
+				else
+					type_of_file=2 # directory
+				fi
+
+				# find if we can find the diff of the file, directories can't have sha1sum.
+				if [ $type_of_file -eq 1 ]
+				then
+					# Take the hash sums, cut out the first column, then take the unique of that. If it's the same, the wc -l will return 1. 2 if not.
+					file_diff=$(sha1sum "$link" "$dest" | awk '{print $1}' | uniq | wc -l)
+
+
+					if [ $file_diff -eq 1 ]
+					then
+						info "Skipping '$dest' because the file has the same hash."
+						same_diff=1
+						skip=1
+					else
+						link_diff=$(sha1sum "$link")
+						dest_diff=$(sha1sum "$dest")
+						same_diff=0
+					fi
+				else
+					same_diff=-1
+				fi
+			fi
+		fi
+
+		# if the file exists and we do not want to skip install it, follow the procedure:
+		if [ $target_exists -eq 1 ] && [ $skip -eq 0 ] && [ $install -eq 1 ]
+		then
+			if [ $type_of_file -eq 2 ]
+			then
+				info "Not-implemented-yet"
+				skip=1
+			else
+				msg="an existing symbolic link"
+				if [ $type_of_file -eq 1 ]
+				then
+					msg="an existing file"
+				elif [ $type_of_file -eq 2 ]
+				then
+					msg="an existing directory"
+				fi
+				info "There seems like '$link' is already $msg and does not link to ${sourcedir}. What would you like to do?"
+				if [ ! -z "$link_diff" ]
+				then
+					info "$link_diff"
+					info "$dest_diff"
+				fi
 				info "[o] overwrite(remove old) [s] skip [a] abort [b] backup(backup old and create new)"
 
 				read_char ans
 				echo ""
 
-				case $ans in 
+				case $ans in
 					o)
-						install=1				
+						install=1
 					;;
 					a)
 						fail "Aborted"
@@ -228,7 +365,7 @@ install_dot_file() {
 				if [ -f "${link}.backup${iterator}" ]
 				then
 					iterator=0
-					
+
 					# if there already is a file with .backup0 and so on, try to figure out if that number is incrementable.
 					while [ ! -f "${link}.backup${iterator}" ]
 					do
@@ -255,41 +392,67 @@ install_dot_file() {
 					fi
 				fi
 
-				
+
 			fi
 
-			if [ "$mode" = $ASK ]
-			then
-				x=""
-				destcopy=$dest
 
-				destcopy=$(basename $destcopy)
-				str=""
-				if [ -d "$dest" ]
+			if [ $target_exists -eq 1 ]
+			then
+				if [ "$same_diff" -eq 1 ]
 				then
-					str="Do you want to install the directory '$destcopy/'? ($valid more to go)"
-				else
-					str="Do you want to install the file '$destcopy'? ($valid more to go)"
-				fi
-				prompt "$str"
-				read_char x
-				echo ""
-				if [ "$x" = $AGREE ]
-				then
-					install=1
-				else 
+					info "The file '$link' is already installed and contains the same diff, skipping."
 					install=0
+				else
+					if [ $install -eq 1 ]
+					then
+						if [ $type_of_file -eq 1 ]
+						then
+							rm "$link"
+							info "Removed '$link'"
+						else
+							rm -r "$link"
+							info "Removed '$link'"
+						fi
+					fi
 				fi
 			fi
 
 			if [ "$install" -eq 1 ]
 			then
-				k=$(ln -sf $dest $link 2>&1)
+				msg=""
+				k=""
+					if [ $no_ln -eq 1 ]
+					then
+						if [ $dest_type_of_file -eq 1 ]
+						then
+							msg="Copied file"
+							k=$(cp $dest $link 2>&1)
+						elif [ $dest_type_of_file -eq 2 ]
+						then
+							msg="Copied directory"
+							info ": $dest $link"
+							k=$(cp -r $dest $link 2>&1)
+						fi
+					else
+						msg="Created symbolic link"
+						k=$(ln -sf $dest $link 2>&1)
+					fi
+
 				if [ -z "$k" ]
 				then
-					success "Created symbolic link '$link' which points to '$dest' ($valid more to go)" 
-				else 
-					info "Error setting the symbolic link for '$dest' which would point to '$link'"
+					success "$msg '$link' which points to '$dest' ($valid more to go)"
+				else
+					case "$dest_type_of_target" in
+					0)
+						info "Error setting the symbolic link for '$dest' which would point to '$link'"
+					;;
+					1)
+						info "Error copying the file '$dest' which would point to '$link'"
+					;;
+					2)
+						info "Error copying the directory '$dest' which would point to '$link'"
+					;;
+					esac
 				fi
 			fi
 		fi # if skip -eq 0
@@ -301,7 +464,8 @@ install_files()
 	info "[DOTFILES] Installing dotfiles"
 	mode="$1"
 	verbose="$2"
-	viminstall 
+	no_ln="$3"
+	viminstall
 	valid=0
 
 	# Look for files that match *.symlink, loop through and see if we can install it.
@@ -326,7 +490,7 @@ install_files()
 		for src in $sources
 		do
 			file=$(basename "$src" | sed "s/\.symlink$//") # sed to remove .symlink
-			install_dot_file "$src" "$symtarget/.$file" "$mode" "$verbose" "$valid"
+			install_dot_file "$src" "$symtarget/.$file" "$mode" "$verbose" "$valid" "$no_ln"
 			valid=`expr $valid - 1`
 		done
 	else
@@ -338,6 +502,7 @@ setup_git_credentials()
 {
 	mode=$1
 	verbose=$2
+	no_ln=$3
 	proceed=1
 	skip_link=0
 
@@ -399,10 +564,10 @@ setup_git_credentials()
 			email=""
 			website=""
 			git_alias=""
-			
+
 			if [ "$mode" = $FORCE ]
-			then 
-				info "[GIT] Setting up git credentials" 
+			then
+				info "[GIT] Setting up git credentials"
 			fi
 			prompt "What is your name? First and last name." "text"
 			read -r name
@@ -416,7 +581,7 @@ setup_git_credentials()
 			read -r git_alias
 			test "$verbose" -eq 1 && info "git_alias set to $git_alias"
 
-			# replace 
+			# replace
 			# "name = " with "name = $name"
 			# and
 			# "email = " with "email = $email"
@@ -437,9 +602,9 @@ setup_git_credentials()
 
 			if [ "$skip_link" -eq 0 ]
 			then
-				install_dot_file "$git_local_path" "$symtarget/.gitconfig.local" " " " " 0
+				install_dot_file "$git_local_path" "$symtarget/.gitconfig.local" " " " " 0 "$no_ln"
 			fi
-		else 
+		else
 			if [ "$proceed" -eq 1 ]
 			then
 				info "Skipped making git credentials because the sample file could not be found."
@@ -469,7 +634,7 @@ uninstall_git_config()
 	mode="$1"
 	proceed=1
 	gitconf_local_symlink="$sourcedir/git/gitconfig.local.symlink"
-	
+
 	if [ -f "$gitconf_local_symlink" ]
 	then
 		if [ "$mode" = $ASK ]
@@ -487,7 +652,7 @@ uninstall_git_config()
 	else
 		proceed=0
 	fi
-	
+
 	if [ "$proceed" -eq 1 ]
 	then
 		rm "$gitconf_local_symlink"
@@ -537,7 +702,7 @@ install_shell_specific()
 			echo ""
 		else
 			info "Installing shell specific items"
-			install=$AGREE 
+			install=$AGREE
 		fi
 
 		if [ "$install" = $AGREE ]
@@ -600,7 +765,7 @@ test_dependencies()
 	done
 
 	# test colors.bash sourcing
-	if [ -f "$sourcedir/dotbin.symlink/colors.sh" ] 
+	if [ -f "$sourcedir/dotbin.symlink/colors.sh" ]
 	then
 		success "Colors.sh exists"
 	else
@@ -615,6 +780,8 @@ main()
 	setting=""
 	choice="install"
 	verbose=0
+  no_ln=0
+
 	for var in "$@"
 	do
 		case "$var"
@@ -633,6 +800,10 @@ main()
 			-v|-vv|-vvv)
 				info "Verbose mode toggled"
 				verbose=1
+      ;;
+      --no-ln)
+        info "Will not use 'ln', fallback to 'cp'"
+        no_ln=1
 			;;
 			-u)
 				choice="uninstall"
@@ -646,7 +817,7 @@ main()
 				exit
 			;;
 		esac
-	done	
+	done
 
 	case "$choice" in
 	install)
@@ -656,24 +827,24 @@ main()
 			read -r p
 			if [ "$p" = $AGREE ]
 			then
-				install_files "-f" "$verbose"
-				setup_git_credentials "-f" "$verbose"
-				install_shell_specific "-f" "$verbose"
+				install_files "-f" "$verbose" "$no_ln"
+				setup_git_credentials "-f" "$verbose" "$no_ln"
+				install_shell_specific "-f" "$verbose" "$no_ln"
 			else
 				info "Aborted"
 			fi
 		else
-			install_files "$setting" "$verbose"
-			setup_git_credentials "$setting" "$verbose"
-			install_shell_specific "$setting" "$verbose"
+			install_files "$setting" "$verbose" "$no_ln"
+			setup_git_credentials "$setting" "$verbose" "$no_ln"
+			install_shell_specific "$setting" "$verbose" "$no_ln"
 		fi
 	;;
 	test)
 		info "Running tests"
-		test_dependencies 
+		test_dependencies
 	;;
 	uninstall)
-		uninstall_dot_files "$setting" "$verbose"
+		uninstall_dot_files "$setting" "$verbose" "$no_ln"
 		uninstall_git_config "$setting" "$verbose"
 		uninstall_shell_specifics "$setting" "$verbose"
 	;;
