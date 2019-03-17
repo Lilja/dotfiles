8a0cdb7abaae27df1a0b51f844606056ca32773e
Erik Lilja <erikvlilja@gmail.com>
2019-03-17 11:58:31 +0100

    [shell] Check XDG for before assigning

--- dotbin/XDG.sh
+++ dotbin/XDG.sh
@@ -1,3 +1,3 @@
-export XDG_CACHE_HOME="${HOME}/.cache"
-export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_DATA_HOME="${HOME}/.local/share"
+[ -z "$XDG_CACHE_HOME" ] && export XDG_CACHE_HOME="${HOME}/.cache"
+[ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="${HOME}/.config"
+[ -z "$XDG_DATA_HOME" ] && export XDG_DATA_HOME="${HOME}/.local/share"
