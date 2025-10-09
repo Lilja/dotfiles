9397f15b3f079889a2fd9bd049aa3c181be04b13
Lilja <erikvlilja@gmail.com>
2025-10-09 11:11:28 +0200

    automatic enable mason - nty

--- nvim/lua/plugins/lsp_init.lua
+++ nvim/lua/plugins/lsp_init.lua
@@ -14,7 +14,9 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup({})
+			require("mason-lspconfig").setup({
+				 automatic_enable = false
+			})
 		end,
 	},
 	{
